642513990fc23edb134364dec8d192822f0c90d3
Neytan Andre Choque Segales <rm88100@estudante.fieb.edu.br>
2023-08-24 18:45:15 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/test/java/com/itb/tcc/MeioAmbiente/MeioAmbienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.MeioAmbiente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeioAmbienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
